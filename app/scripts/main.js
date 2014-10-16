da5f94a98a58997dfa5c8ae075dde568c99ae83d
f.monteiro <fernando@newaeonweb.com.br>
2014-10-16 10:53:55 -0300

    Add toastr lib to message warnings

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,20 +2,19 @@ require.config({
   paths: {
     'text': '../bower_components/requirejs-text/text',
     'jquery': '../bower_components/jquery/jquery.min',
-    'kendo': '../bower_components/kendo-ui/js/kendo.web.min'
-
+      'kendo': '../bower_components/kendo-ui/js/kendo.web.min',
+      'toastr': '../bower_components/toastr/toastr.min'
   },
   // inform requirejs that kendo ui depends on jquery
   shim: {
-    "kendo": {
-      deps: ["jquery"]
-    }
+      "kendo": { deps: ["jquery"] },
+      "toastr": { deps: ["jquery"] }
   }
 });
 
 require([
   'app'
-], function (app, jquery, kendo) {
+], function (app) {
 
   app.init();
 
--- app/scripts/views/users/users.js
+++ app/scripts/views/users/users.js
@@ -1,16 +1,25 @@
+var user = user || {};
+
 define([
   // Require.js text plugin loads the HTML template pages​
   'text!views/users/users.html',
   'jquery',
-  'kendo'
-], function (template) {
+    'kendo',
+    'toastr'
+], function (template, $, kendo, toastr) {
 
   'use strict';
 
     template = $.trim(template);
 
-    var userModel = kendo.observable({
+    user.viewModel = kendo.observable({
 
+        // Set viewModel properties
+        username: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        urlService: 'http://localhost:3000/api/users/',
 
       listUsers: new kendo.data.DataSource({
         schema: {
@@ -43,14 +52,56 @@ define([
           }
         }
 
-      })
+      }),
+
+        // Event
+        submitForm: function (e) {
+            e.preventDefault();
+            //console.log('click', login.viewModel.username);
+            $.ajax({
+                type: 'POST',
+                url: user.viewModel.urlService,
+                timeout: 30000,
+                dataType: "json",
+                data: {
+                    username: user.viewModel.username,
+                    firstName: user.viewModel.firstName,
+                    lastName: user.viewModel.lastName,
+                    email: user.viewModel.email
+                },
+                contentType: 'application/x-www-form-urlencoded; charset=ISO 8859-1',
+                beforeSend: function () {
+
+                },
+                complete: function () {
+
+                },
+                success: function (result) {
+                    console.log(result.message);
+                    // Update Grid after insert new user
+
+                    toastr.success(result.message);
+                    $("#gridRestful").data("kendoGrid").dataSource.read();
+                },
+                error: function (xhr) {
+                    $('#show_combo').html('<p class="destaque">Lamento! Ocorreu um erro. Por favor tente mais tarde.');
+                    console.log(xhr);
+                }
+            });
+        },
+
+        editDetails: function (e) {
+            e.preventDefault();
+            var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
+            console.log(dataItem._id);
+        }
 
     });
 
     // Bind the viewModel
-    kendo.bind($('#viewModelRemote'), userModel);
+    kendo.bind($('#addUserForm'), user.viewModel);
 
-    var view = new kendo.View(template, { model: userModel });
+    var view = new kendo.View(template, { model: user.viewModel });
 
     return view;
 
